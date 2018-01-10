af70af69b9177f1a871116169845e8668db3685b
Ni Chia <nichia@gmail.com>
2018-01-10 17:55:23 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -61,7 +61,7 @@ class MusicLibraryController
     if (artist = Artist.find_by_name(artist_name)) != nil
       sorted_songs =  artist.songs.sort_by { |song| song.name}
       sorted_songs.each { |song|
-        puts "#{sorted_songs.index(song) + 1}. #{song.name}"
+        puts "#{sorted_songs.index(song) + 1}. #{song.name} - #{song.genre.name}"
       }
     end
   end
